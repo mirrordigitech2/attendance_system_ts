e8c1fa48025d55262a2d921cce8509e8c8172569
razanbarakat <razan.alfeelat@gmail.com>
2024-06-06 21:17:46 +0300

    fix select done

--- src/app/(loggedin)/schools/components/Form.tsx
+++ src/app/(loggedin)/schools/components/Form.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { School, SchoolForm } from "@/lib/types";
@@ -17,9 +18,9 @@ import {
 import { Input } from "@/components/ui/input";
 import {
   Select,
+  SelectTrigger,
   SelectContent,
   SelectItem,
-  SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 import { useUsers } from "../../users/hooks/useUsers";
@@ -29,139 +30,132 @@ type Props = {
   item?: School | null;
 };
 
-export const FormSchools = ({ onClose, item }: Props) => {
+export const FormSchools = (props: Props) => {
   const { users } = useUsers();
+  const itemId = props.item?.id;
 
   const form = useForm<SchoolForm>({
     resolver: zodResolver(SchoolSchema),
-    defaultValues: item ? { ...item, lecturer: item.lecturer || "" } : {},
+    ...(props.item?.id && { defaultValues: { ...props.item } }),
   });
-
+  console.log("item", props.item?.id);
   const submitData = async (data: SchoolForm) => {
+    console.log("data", data);
     try {
-      if (item?.id) {
-        // Edit existing school document
-        await setDoc(doc(db, "schools", item.id), { ...data });
+      if (itemId) {
+        await setDoc(doc(db, "schools", itemId), { ...data });
       } else {
-        // Add new school document
         await addDoc(collection(db, "schools"), { ...data });
       }
-      onClose();
+      props.onClose();
     } catch (error) {
-      console.error("Error adding or updating document: ", error);
+      console.error("Error adding document: ", error);
     }
   };
 
   return (
     <div>
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(submitData)}
-          className="grid grid-cols-3 pb-2 mt-1 space-x-2 w-90"
-        >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem className="">
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input {...field} type="text" placeholder="" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="location"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Location</FormLabel>
-                <FormControl>
-                  <Input {...field} type="text" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="lecturer"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Lecturer</FormLabel>
-                <FormControl>
-                  <Select
-                    // onValueChange={(value) => field.onChange(value)}
-                    onValueChange={field.onChange}
-                    defaultValue={field.value.name}
-                  >
-                    <SelectTrigger>
-                      <SelectValue>
-                        {field.value.name}
-                        {/* {item && item !== null
-                          ? item.lecturer.name
-                          : "Select a lecturer"} */}
-                        {/* {users.find((user) => user.id === field.value)?.name ||
-                          "Select Lecturer"} */}
-                      </SelectValue>
-                    </SelectTrigger>
-                    <SelectContent>
-                      {users.map((user) => (
-                        <SelectItem key={user.id} value={user.id}>
-                          {user.name}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="manager"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Manager</FormLabel>
-                <FormControl>
-                  <Input {...field} type="text" placeholder="" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Phone</FormLabel>
-                <FormControl>
-                  <Input {...field} type="number" placeholder="" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="totalStudent"
-            render={({ field }) => {
-              return (
+        <form onSubmit={form.handleSubmit(submitData)} className="">
+          <div className="grid grid-cols-3 pb-2 mt-1 space-x-2 w-90">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="text" placeholder="" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Total Student</FormLabel>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="text" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lecturer"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Lecturer</FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value ? field.value.name : ""}
+                    >
+                      <SelectTrigger>
+                        <SelectValue>
+                          {field.value ? field.value.name : ""}
+                        </SelectValue>
+                      </SelectTrigger>
+                      <SelectContent>
+                        {users.map((user) => (
+                          <SelectItem key={user.id} value={user.id}>
+                            {user.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="manager"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Manager</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="text" placeholder="" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone</FormLabel>
                   <FormControl>
                     <Input {...field} type="number" placeholder="" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
-              );
-            }}
-          />
-          <Button type="submit" className="w-full col-span-3">
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="totalStudent"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Total Student</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="number" placeholder="" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </div>
+          <Button type="submit" className="w-full">
             Submit
           </Button>
         </form>
